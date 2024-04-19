d4e4fefbbc48cc8df94811f678d9f5faa5e3f146
Khushal Khan <khushalkhan40@gmail.com>
2024-04-19 17:15:21 +0500

    homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import HomePage from "./Container/HomePage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 };
 
 export default App;
